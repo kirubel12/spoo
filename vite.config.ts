d961991a647e3a235e279ef46b042ec0011d2b9d
kirubel12 <kirubelhabte1991@gmail.com>
2024-08-02 13:24:49 +0300

    tailwind & shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
